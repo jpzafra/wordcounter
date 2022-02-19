ad096be80c8ca25a4e6e1e59c8030b40a315b618
jpzafra <jpzafra@gmail.com>
2022-02-19 14:53:25 +0100

    second

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface wordCounterPair {
+  word: string,
+  counter: number
+}
 
 @Component({
   selector: 'app-home',
@@ -7,6 +13,43 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  text: string;
+
+  result: wordCounterPair[] = [];
+
+  numWords: number;
+
   constructor() {}
+
+  countWords(){
+
+    this.result = [];
+
+    const words = this.text.split(' ');
+    this.numWords = words.length;
+    
+    words.forEach( (word) => {
+      let arr2 = words.filter(element => element === word).length;
+
+      if(!this.result.find(element => element.word === word)){
+        this.result.push(
+          {
+            word: word, 
+            counter: arr2
+          }
+        )
+      }
+
+    });
+
+    this.result.sort((a, b) => (a.word < b.word ? -1 : 1));
+    this.result.sort((a, b) => (a.counter > b.counter ? -1 : 1));
+
+  }
+
+  clean(){
+    this.text = "";
+    this.result = [];
+  }
 
 }
